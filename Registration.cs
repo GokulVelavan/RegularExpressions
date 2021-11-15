7e07896264b584572a7a498d987d3d45c27dd705
GokulVelavan <gokulvelavans@gmail.com>
2021-11-15 13:51:03 +0530

    mobile number validation

--- Registration.cs
+++ Registration.cs
@@ -56,5 +56,22 @@ namespace RegularExoressions
                 Console.WriteLine("emailis not valid");
             }
         }
+
+
+        public void MobileNumber()
+        {
+            string mobile = "^([0-9]{2}[ ][0-9]{10})|([0-9]{3}[ ][0-9]{10})$";
+            Console.WriteLine("Enter your Mobile Number:");
+            string number = Console.ReadLine();
+            Regex regex = new Regex(mobile);
+            if (regex.IsMatch(number))
+            {
+                Console.WriteLine("Mobile number is valid");
+            }
+            else
+            {
+                Console.WriteLine(" Mobile Number is not valid");
+            }
+        }
     }
 }
